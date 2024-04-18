fdca3d58179ab2fc2167224aec80f9ef1ed86df1
RennanJSM <87147821+RennanJSM@users.noreply.github.com>
2024-04-18 14:57:07 -0300

    .

---
+++ lib/pages/perfil.page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class PerfilPage extends StatefulWidget {
+  const PerfilPage({super.key});
+  
+  static const routeName = '/perfil';
+
+  @override
+  State<PerfilPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<PerfilPage> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+}
