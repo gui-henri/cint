85ae10923009fd5726a41cf9a2f7cb137f3e2cff
infodoug <douglasrafael.info@gmail.com>
2024-04-24 14:36:09 -0300

    config botões

--- lib/pages/apresentacao/apresentacaopage.dart
+++ lib/pages/apresentacao/apresentacaopage.dart
@@ -35,20 +35,29 @@ class _ApresentacaoPageState extends State<ApresentacaoPage> {
 
   @override
   Widget build(BuildContext context) {
+    // Tela principal, que recebe os widgets e as páginas 1 2 e 3
     return Scaffold(
       appBar: initialAppBar(),
-      body: PageView.builder(
-        controller: _pageController,
-        itemCount: telas.length,
-        itemBuilder: (_, i) {
-          return telas[i];
-        },
-        onPageChanged: (int index) {
-          setState(() {
-            currentPageIndex = index;
-          });
-        },
-      ),
+      body: Stack(children: [
+        screenPages(),
+        pageTracker(),
+        linhaInferior(),
+      ]),
+    );
+  }
+
+  PageView screenPages() {
+    return PageView.builder(
+      controller: _pageController,
+      itemCount: telas.length,
+      itemBuilder: (_, i) {
+        return telas[i];
+      },
+      onPageChanged: (int index) {
+        setState(() {
+          currentPageIndex = index;
+        });
+      },
     );
   }
 
@@ -99,5 +108,89 @@ class _ApresentacaoPageState extends State<ApresentacaoPage> {
         color: Color(0xFF28730E),
       ),
     );
+  }
+
+  Widget pageTracker() {
+    return Align(
+      alignment: Alignment.bottomCenter,
+      child: Container(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: List.generate(
+            telas.length,
+            (index) => buildDot(index, context),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Container buildDot(int index, BuildContext context) {
+    return Container(
+      height: 10,
+      width: 10,
+      margin: EdgeInsets.only(left: 5, bottom: 40),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        color: (currentPageIndex == index)
+            ? Theme.of(context).primaryColor
+            : Colors.grey,
+      ),
+    );
+  }
+
+  Widget linhaInferior() {
+    return Align(
+      alignment: Alignment.bottomCenter,
+      child: Container(
+        child: Row(
+          children: [
+            (currentPageIndex != 2) ? botaoPular() : Spacer(),
+            if (currentPageIndex == 2) botaoComecar(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Padding modelobotaoInferior(String text) {
+    return Padding(
+      padding: const EdgeInsets.all(11.0),
+      child: Container(
+        width: (currentPageIndex != 2) ? 70 : 100,
+        child: TextButton(
+          onPressed: () {
+            Navigator.pushNamed(context, '/home');
+          },
+          style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all<Color>(Colors.white),
+            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+              RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(7.0),
+                side: BorderSide(
+                  width: 1,
+                  color: Theme.of(context).primaryColor,
+                ),
+              ),
+            ),
+          ),
+          child: Text(
+            text,
+            style: TextStyle(
+              color: Theme.of(context).primaryColor,
+              fontSize: 16.0,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  botaoPular() {
+    return modelobotaoInferior('Pular');
+  }
+
+  botaoComecar() {
+    return modelobotaoInferior('Começar');
   }
 }
